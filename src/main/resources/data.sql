f37cfab98083ebe2dabbec82799d227c5d4e4bf4
alejocerutti4 <alejocerutti4@gmail.com>
2023-05-16 20:31:37 -0300

    more dump data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,6 +13,16 @@ INSERT INTO recurso (id, nombre, descripcion) VALUES ('11', 'Sofá', 'Sofá de c
 INSERT INTO recurso (id, nombre, descripcion) VALUES ('12', 'Proyector HD', 'Proyector HD marca Epson');
 INSERT INTO recurso (id, nombre, descripcion) VALUES ('13', 'Pizarra interactiva', 'Pizarra interactiva SMART Board');
 INSERT INTO recurso (id, nombre, descripcion) VALUES ('14', 'Computadora de escritorio', 'Dell OptiPlex 3070');
+INSERT INTO recurso (id, nombre, descripcion) VALUES ('16', 'Impresora', 'Impresora láser');
+INSERT INTO recurso (id, nombre, descripcion) VALUES ('17', 'Pizarra magnética', 'Pizarra magnética negra');
+INSERT INTO recurso (id, nombre, descripcion) VALUES ('18', 'Silla ergonómica', 'Silla ergonómica ajustable');
+INSERT INTO recurso (id, nombre, descripcion) VALUES ('19', 'Escáner', 'Escáner de alta resolución');
+INSERT INTO recurso (id, nombre, descripcion) VALUES ('20', 'Mesa plegable', 'Mesa plegable de plástico');
+INSERT INTO recurso (id, nombre, descripcion) VALUES ('21', 'Proyector 4K', 'Proyector 4K de última generación');
+INSERT INTO recurso (id, nombre, descripcion) VALUES ('22', 'Pizarra de corcho', 'Pizarra de corcho para notas');
+INSERT INTO recurso (id, nombre, descripcion) VALUES ('23', 'Computadora iMac', 'Apple iMac Pro');
+INSERT INTO recurso (id, nombre, descripcion) VALUES ('24', 'Atril', 'Atril de metal ajustable');
+INSERT INTO recurso (id, nombre, descripcion) VALUES ('25', 'Mesa de conferencia', 'Mesa de conferencia para reuniones');
 
 -- Tabla de espacios físicos
 INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('1', 'SUM', 'Salón de usos múltiples', '300');
@@ -30,6 +40,16 @@ INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('12', 'S
 INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('13', 'Sala de exposiciones', 'Sala de exposiciones de arte', '80');
 INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('14', 'Estudio de música', 'Estudio de grabación de música', '5');
 INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('15', 'Sala de ensayo', 'Sala de ensayo de teatro', '15');
+INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('16', 'Laboratorio de Física', 'Laboratorio de Física avanzada', '25');
+INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('17', 'Aula 401', 'Aula de cuarto año', '35');
+INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('18', 'Sala de estudio', 'Sala de estudio silenciosa', '50');
+INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('19', 'Sala de descanso 2', 'Sala de descanso adicional', '15');
+INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('20', 'Estudio de fotografía', 'Estudio de fotografía profesional', '10');
+INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('21', 'Sala de ensayo 2', 'Sala de ensayo de música', '8');
+INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('22', 'Aula 501', 'Aula de quinto año', '30');
+INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('23', 'Sala de conferencias 2', 'Sala de conferencias amplia', '200');
+INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('24', 'Aula 601', 'Aula de sexto año', '25');
+INSERT INTO espacio_fisico (id, nombre, descripcion, capacidad) VALUES ('25', 'Sala de exposiciones 2', 'Sala de exposiciones de arte adicional', '100');
 
 -- Tabla de relación entre espacios físicos y recursos
 INSERT INTO espacio_fisico_recursos (espacio_fisico_id, recursos_id) VALUES ('1', '1');
@@ -47,3 +67,10 @@ INSERT INTO espacio_fisico_recursos (espacio_fisico_id, recursos_id) VALUES ('12
 INSERT INTO espacio_fisico_recursos (espacio_fisico_id, recursos_id) VALUES ('13', '13');
 INSERT INTO espacio_fisico_recursos (espacio_fisico_id, recursos_id) VALUES ('14', '14');
 INSERT INTO espacio_fisico_recursos (espacio_fisico_id, recursos_id) VALUES ('15', '14');
+INSERT INTO espacio_fisico_recursos (espacio_fisico_id, recursos_id) VALUES ('16', '6');
+INSERT INTO espacio_fisico_recursos (espacio_fisico_id, recursos_id) VALUES ('17', '9');
+INSERT INTO espacio_fisico_recursos (espacio_fisico_id, recursos_id) VALUES ('18', '4');
+INSERT INTO espacio_fisico_recursos (espacio_fisico_id, recursos_id) VALUES ('19', '10');
+INSERT INTO espacio_fisico_recursos (espacio_fisico_id, recursos_id) VALUES ('20', '12');
+INSERT INTO espacio_fisico_recursos (espacio_fisico_id, recursos_id) VALUES ('21', '13');
+INSERT INTO espacio_fisico_recursos (espacio_fisico_id, recursos_id) VALUES ('22', '17');
